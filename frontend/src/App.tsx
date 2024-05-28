d99ed32ff044fcd7fe232d7e714e51820c480a07
SanjayM-2002 <sanjaymshivakripa@gmail.com>
2024-05-28 19:38:15 +0530

    added home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,12 +6,14 @@ import Login from './pages/Login';
 import BulkBlogs from './pages/BulkBlogs';
 import Blog from './pages/Blog';
 import Publish from './pages/Publish';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Home />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/login' element={<Login />} />
           <Route path='/blogs' element={<BulkBlogs />} />
